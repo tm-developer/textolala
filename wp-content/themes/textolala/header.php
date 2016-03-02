2424004d9ab87844133f28def3bc96170b32e828
U-Barbichette\Nuno Nico <thomas.monicault@gmail.com>
2016-03-02 17:41:36 +0100

    bug pop-up

--- wp-content/themes/textolala/header.php
+++ wp-content/themes/textolala/header.php
@@ -12,8 +12,8 @@
 		<link rel="author" href="">
 		<?php wp_head() ?>
     </head>
-    <body <?php body_class() ?> id="<?php if (is_page('home')) { echo "home"; }elseif( is_category()){$category = get_the_category(); echo $category[0]->slug;}elseif (is_page('top')) { echo "top"; }elseif (is_page('hasard')) { echo "hasard"; }?>">
-    	<div class="loader <?php if (is_page('home')) { echo "home"; }elseif( is_category()){$category = get_the_category(); echo $category[0]->slug;}elseif (is_page('top')) { echo "top"; }elseif (is_page('hasard')) { echo "hasard"; }?>">
+    <body <?php body_class() ?> id="<?php if(is_page('home')) { echo "home"; }elseif( is_category()){$category = get_the_category(); echo $category[0]->slug;}elseif (is_page('top')) { echo "top"; }elseif (is_page('hasard')) { echo "hasard"; }?>">
+    	<div class="loader <?php if(is_page('home')) { echo "home"; }elseif( is_category()){$category = get_the_category(); echo $category[0]->slug;}elseif (is_page('top')) { echo "top"; }elseif (is_page('hasard')) { echo "hasard"; }?>">
     		
     	</div>
     	
@@ -42,7 +42,7 @@
 			</nav>
 		</header>
 
-		<div class="pop-up <?php $category = get_the_category(); echo $category[0]->slug; if (is_page('top')) { echo "top"; }elseif (is_page('hasard')) { echo "hasard"; }?>">
+		<div class="pop-up <?php if (is_page('home')) { echo "home"; }elseif( is_category()){$category = get_the_category(); echo $category[0]->slug;}elseif (is_page('top')) { echo "top"; }elseif (is_page('hasard')) { echo "hasard"; }?>">
 			<a href="" class="cross"></a>
 			<?php echo do_shortcode('[contact-form-7 id="4" title="Formulaire de contact 1"]'); ?>
 		</div>
